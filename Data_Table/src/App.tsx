499b40ab807f925ed17f92ae13f2b5aa43c9a1bd
bhavishyajangid <bhavishya8824@gmail.com>
2025-11-07 18:37:12 +0530

    complete

--- Data_Table/src/App.tsx
+++ Data_Table/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import Table from "./components/Table";
 import { useDispatch, useSelector } from "react-redux";
